9721358e20c179312e4c829122e12513b5045ccd
Dingu Sagar <dingu.sagar@engati.com>
2023-01-27 03:32:40 +0530

    fixing open folder command in ubuntu

--- gesture_mapping.py
+++ gesture_mapping.py
@@ -4,6 +4,6 @@ from commands import CLICommand, PythonBasedCommand
 GestureMapping = {
     'rectangle': CLICommand("gnome-calculator", description="Open Calculator"),
     'circle': CLICommand("google-chrome https://www.google.com/", description="Open google in chrome"),
-    'triangle': CLICommand("open /media/dingusagar/Data/", description="Open Data Folder")
+    'triangle': CLICommand("xdg-open /media/dingusagar/Data/", description="Open Data Folder")
 }
 
